b8c63aa92d79d5a7e482193977dcf496dea620e1
PaulRB04 <paul.rb2004@gmail.com>
2024-11-26 22:49:23 +0100

    POINTCOUNTEEEER

---
+++ RaBPaulAlex_Project/Assets/RaBURP_Root/Scripts/PointCounter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PointCounter : MonoBehaviour
+{
+
+    public int point = 0;
+
+    public TextMeshProUGUI pointText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.transform.tag == "PickUp")
+        {
+            point++;
+            pointText.text = "PESCADOS: " + point.ToString();
+            Debug.Log(point);
+            Destroy(other.gameObject);
+        }
+
+    }
+
+}
