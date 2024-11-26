4165dd4ccdf0ec3722b7c5f7683898e3cc70cded
PaulRB04 <paul.rb2004@gmail.com>
2024-11-26 20:36:01 +0100

    velocidad y demas

--- RaBPaulAlex_Project/Assets/RaBURP_Root/Scripts/PlayerController.cs
+++ RaBPaulAlex_Project/Assets/RaBURP_Root/Scripts/PlayerController.cs
@@ -88,6 +88,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("PlataformaMovible"))
         {
             transform.parent = collision.transform;
+            isGrounded = true;
         }
 
        
